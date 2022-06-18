1461678b8b6fc952d5d21bd34958b1bea89b67fd
arfan <shaifullaharfan17@gmail.com>
2022-06-18 23:42:42 +0600

    tasklist and functions

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './Layout.module.scss'
+
+
+function Layout({ children }) {
+  return (
+    <main className={classes.container}>{children}</main>
+  )
+}
+
+export default Layout
---
+++ frontend/src/components/index.jsx
@@ -0,0 +1,2 @@
+export { default as Navbar} from './nav/Navbar';
+export { default as TaskList } from './task/TaskList';
