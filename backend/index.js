ab52443475985ac0cc04f6d0552310ef4933284c
arfan <shaifullaharfan17@gmail.com>
2022-06-20 01:48:56 +0600

    setting  cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  })
+);
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser());
