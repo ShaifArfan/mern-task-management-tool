e3e37412fcc470ffa6599b5f5ba12cd78e1c8f72
arfan <shaifullaharfan17@gmail.com>
2022-06-20 21:23:15 +0600

    setting cors for multiple origins from env

--- backend/index.js
+++ backend/index.js
@@ -8,13 +8,24 @@ import allRoutes from './routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
-const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
+const CLIENT_URL_STRING = process.env.CLIENT_URL || 'http://localhost:3000';
+
+const allowedDomains = CLIENT_URL_STRING.split(', ');
 
 // middleware
 app.use(
   cors({
     credentials: true,
-    origin: CLIENT_URL,
+    origin(origin, callback) {
+      // bypass the requests with no origin (like curl requests, mobile apps, etc )
+      if (!origin) return callback(null, true);
+
+      if (allowedDomains.indexOf(origin) === -1) {
+        const msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
   })
 );
 app.use(morgan('tiny'));
