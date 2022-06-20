59cac1a7e56188b9eec9e8217ab4bea56ff6f9d0
arfan <shaifullaharfan17@gmail.com>
2022-06-21 00:39:49 +0600

    adding api url fron env

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   },
   rules: {
     // base rules
+    'no-underscore-dangle': 0,
     'no-alert': 0,
     'no-unused-vars': [
       1,
--- frontend/src/utils/getApiBaseUrl.js
+++ frontend/src/utils/getApiBaseUrl.js
@@ -1,3 +1,7 @@
-const getApiBaseUrl = () => import.meta.env.VITE_API_BASE_URL;
-
+const getApiBaseUrl = () => {
+  if (process.env.NODE_ENV === 'development') {
+    return 'http://localhost:8000';
+  }
+  return import.meta.env.VITE_API_BASE_URL;
+};
 export default getApiBaseUrl;
